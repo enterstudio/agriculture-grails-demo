f45e631c22e2e119d36a435c2d63a3b4847ff4f9
mattmorten <mattmorten@googlemail.com>
2015-06-17 14:16:23 -0400

    Improve UI styling. Convert tabs to spaces

--- grails-app/controllers/flowthings/grails/demo/AgricultureController.groovy
+++ grails-app/controllers/flowthings/grails/demo/AgricultureController.groovy
@@ -24,88 +24,88 @@ import com.flowthings.client.exception.FlowthingsException
 class AgricultureController {
 
 
-	
-	/**
-	 * Set up to use the flowthings.io REST api.
-	 */
-	def creds = Credentials.fromBluemixOrDefault(new Credentials("<your flowthingsio account name>", "<your master token>"))
-	def api = new RestApi(creds)
-	
-	def account = creds.account
-	
-	def basePath = "/$account/agriculture"
-	def inputPath = "/$account/agriculture/inputs"
-	def outputPath = "/$account/agriculture/commands"
-	
-	Flow baseFlow;
-	Flow inputFlow;
-	Flow outputFlow;
-	Token deviceToken;
-	
-	def rainSimulator, moistureSimulator, temperatureSimulator;
-	def scheduler = Executors.newScheduledThreadPool(5);
-	
-	
-	/**
-	 * On app startup, check if the flowthings.io objects have been created. If they have,
-	 * go and grab references to them. Otherwise we need to create... 
-	 */
-	def getFlowsAndToken () {
-		api.send(flow().find(new QueryOptions().filter("path==\"${inputPath}\""))).each {
-			flow -> inputFlow = flow;
-			println "Got Input Flow : ${inputFlow.getId()}"
-		}
-		
-		if (inputFlow == null){
-			setup()
-		} else {
-			api.send(flow().find(new QueryOptions().filter("path==\"${outputPath}\""))).each {
-				flow -> outputFlow = flow;
-				println "Got Output Flow : ${outputFlow.getId()}"
-			}
-			api.send(token().find(new QueryOptions().filter("description==\"device token\""))).each {
-				token -> deviceToken = token;
-				println "Got Device Token : ${deviceToken.getId()}"
-			}
-		}
-		
-		if (rainSimulator == null){
-			startSimulatingData()
-		}
-	}
-	
-	/**
-	 * Create the flowthings.io event processing application 
-	 */
-    def setup () { 
-		
-		/**
-		 * Create Flow objects
-		 */
-		baseFlow = new Flow.Builder().setPath(basePath).get()
-		inputFlow = new Flow.Builder().setPath(inputPath).get()
-		outputFlow = new Flow.Builder().setPath(outputPath).get()
-		
-		try {
-			baseFlow = api.send(flow().create(baseFlow))
-			inputFlow = api.send(flow().create(inputFlow))
-			outputFlow = api.send(flow().create(outputFlow))
-			
-			/**
-			 * Define JS code to be executed on Flowthings.io in the cloud
-			 * The logic works like this:
-			 * 
-			 * 1. Readings from soil, temperature, and weather sensors (simulated)
-			 * are sent directly to the /<account>/agriculture/input Flow
-			 * 
-			 * 2. Each time a reading arrives, the below JS is run on flowthings.io
-			 * to decide whether or not to switch on the watering system.
-			 * 
-			 * 3. A command is sent to turn on or off the watering system to
-			 * the /<account>/agriculture/commands Flow
-			 *
-			 */
-			def js = """
+
+  /**
+   * Set up to use the flowthings.io REST api.
+   */
+  def creds = Credentials.fromBluemixOrDefault(new Credentials("<your flowthingsio account name>", "<your master token>"))
+  def api = new RestApi(creds)
+
+  def account = creds.account
+
+  def basePath = "/$account/agriculture"
+  def inputPath = "/$account/agriculture/inputs"
+  def outputPath = "/$account/agriculture/commands"
+
+  Flow baseFlow;
+  Flow inputFlow;
+  Flow outputFlow;
+  Token deviceToken;
+
+  def rainSimulator, moistureSimulator, temperatureSimulator;
+  def scheduler = Executors.newScheduledThreadPool(5);
+
+
+  /**
+   * On app startup, check if the flowthings.io objects have been created. If they have,
+   * go and grab references to them. Otherwise we need to create... 
+   */
+  def getFlowsAndToken () {
+    api.send(flow().find(new QueryOptions().filter("path==\"${inputPath}\""))).each { flow ->
+      inputFlow = flow;
+      println "Got Input Flow : ${inputFlow.getId()}"
+    }
+
+    if (inputFlow == null){
+      setup()
+    } else {
+      api.send(flow().find(new QueryOptions().filter("path==\"${outputPath}\""))).each { flow ->
+        outputFlow = flow;
+        println "Got Output Flow : ${outputFlow.getId()}"
+      }
+      api.send(token().find(new QueryOptions().filter("description==\"device token\""))).each { token ->
+        deviceToken = token;
+        println "Got Device Token : ${deviceToken.getId()}"
+      }
+    }
+
+    if (rainSimulator == null){
+      startSimulatingData()
+    }
+  }
+
+  /**
+   * Create the flowthings.io event processing application 
+   */
+  def setup () {
+
+    /**
+     * Create Flow objects
+     */
+    baseFlow = new Flow.Builder().setPath(basePath).get()
+    inputFlow = new Flow.Builder().setPath(inputPath).get()
+    outputFlow = new Flow.Builder().setPath(outputPath).get()
+
+    try {
+      baseFlow = api.send(flow().create(baseFlow))
+      inputFlow = api.send(flow().create(inputFlow))
+      outputFlow = api.send(flow().create(outputFlow))
+
+      /**
+       * Define JS code to be executed on Flowthings.io in the cloud
+       * The logic works like this:
+       * 
+       * 1. Readings from soil, temperature, and weather sensors (simulated)
+       * are sent directly to the /<account>/agriculture/input Flow
+       * 
+       * 2. Each time a reading arrives, the below JS is run on flowthings.io
+       * to decide whether or not to switch on the watering system.
+       * 
+       * 3. A command is sent to turn on or off the watering system to
+       * the /<account>/agriculture/commands Flow
+       *
+       */
+      def js = """
 			function (input) {		
 				var thisCategory = input.fhash;
 				
@@ -133,59 +133,58 @@ class AgricultureController {
 				var alert = { fhash : "waterOn", elems : { command : shouldWater } };
 				return alert;
 			}
-			""" 
-			
-			def commandTrack = new Track.Builder()
-			.setSource(inputPath)
-			.setDestination(outputPath)
-			.setJs(js)
-			.get()
-			
-			commandTrack = api.send(track().create(commandTrack))
-			
-			
-			/**
-			 * Create a limited token for our devices, so they
-			 * don't have unlimited access to our app which would
-			 * be dangerous
-			 */
-			deviceToken = new Token.Builder()
-				.addPath(inputFlow.getPath(),new TokenPermissions(true, true))
-				.addPath(outputFlow.getPath(),new TokenPermissions(true, false))
-				.setDescription("device token").get();
-			deviceToken = api.send(token().create(deviceToken))
-			
-		} catch (FlowthingsException e) {
-			e.printStackTrace()
-			render "There was a problem!"
-		}
-	}
-	
-	/**
-	 * This is just a demo - we don't have real sensors sending us data. So
-	 * start some tasks to periodically send sample datsa. 
-	 */
-	def startSimulatingData(){
-		def credentials = new Credentials(account, deviceToken.getTokenString())
-		rainSimulator = scheduler.scheduleAtFixedRate(
-			new RainForecastSimulator(credentials, inputFlow, outputFlow), 0, 2, TimeUnit.SECONDS)
-		moistureSimulator = scheduler.scheduleAtFixedRate(
-			new MoistureSimulator(credentials, inputFlow, outputFlow), 0, 2, TimeUnit.SECONDS)
-		temperatureSimulator = scheduler.scheduleAtFixedRate(
-			new TemperatureSimulator(credentials, inputFlow, outputFlow), 0, 2, TimeUnit.SECONDS)
-	}
-	
-	/**
-	 * Render the main page
-	 */
-	def index(){
-		if (inputFlow == null){
-			getFlowsAndToken()
-		}
-		
-		render(view : "index", model : [account : account, token : deviceToken.getTokenString(),
-			inputPath : inputPath, outputPath : outputPath])
-	}
+			"""
+
+      def commandTrack = new Track.Builder()
+          .setSource(inputPath)
+          .setDestination(outputPath)
+          .setJs(js)
+          .get()
+
+      commandTrack = api.send(track().create(commandTrack))
+
+
+      /**
+       * Create a limited token for our devices, so they
+       * don't have unlimited access to our app which would
+       * be dangerous
+       */
+      deviceToken = new Token.Builder()
+          .addPath(inputFlow.getPath(),new TokenPermissions(true, true))
+          .addPath(outputFlow.getPath(),new TokenPermissions(true, false))
+          .setDescription("device token").get();
+      deviceToken = api.send(token().create(deviceToken))
+    } catch (FlowthingsException e) {
+      e.printStackTrace()
+      render "There was a problem!"
+    }
+  }
+
+  /**
+   * This is just a demo - we don't have real sensors sending us data. So
+   * start some tasks to periodically send sample datsa. 
+   */
+  def startSimulatingData(){
+    def credentials = new Credentials(account, deviceToken.getTokenString())
+    rainSimulator = scheduler.scheduleAtFixedRate(
+        new RainForecastSimulator(credentials, inputFlow, outputFlow), 0, 2, TimeUnit.SECONDS)
+    moistureSimulator = scheduler.scheduleAtFixedRate(
+        new MoistureSimulator(credentials, inputFlow, outputFlow), 0, 2, TimeUnit.SECONDS)
+    temperatureSimulator = scheduler.scheduleAtFixedRate(
+        new TemperatureSimulator(credentials, inputFlow, outputFlow), 0, 2, TimeUnit.SECONDS)
+  }
+
+  /**
+   * Render the main page
+   */
+  def index(){
+    if (inputFlow == null){
+      getFlowsAndToken()
+    }
+
+    render(view : "index", model : [account : account, token : deviceToken.getTokenString(),
+      inputPath : inputPath, outputPath : outputPath])
+  }
 }
 
 /**
@@ -194,113 +193,112 @@ class AgricultureController {
  * the moisture value is reset to maximum
  */
 class MoistureSimulator extends Simulator {
-	
-	def moisture = 50;
-	
-	def MoistureSimulator(Credentials credentials, Flow inputFlow, Flow outputFlow){
-		super(credentials, inputFlow, outputFlow)
-		websocketApi.send(drop(outputFlow.getId()).subscribe({
-			Drop drop -> 
-			if ("waterOn".equals(drop.getFhash()) && drop.getElems().get("command") == true){
-				// The plant has been watered
-				moisture = 50;
-				println "Received water command, resetting moisture"
-			}
-		}));
-	}
-	
-	@Override
-	public void run() {
-		try {
-			// Decrease moisture
-			moisture = Math.max(0, moisture - 5);
-			
-			// Send
-			println "Sending Moisture: ${moisture}"
-			websocketApi.send(drop(inputFlow.getId()).create(moistureData())).get()
-		} catch (Exception e) {
-			e.printStackTrace()
-		}
-	}
-	
-	def moistureData() {
-		return new Drop.Builder().setFhash("soilMoisture").addElem("reading",moisture).get()
-	}
+
+  def moisture = 50;
+
+  def MoistureSimulator(Credentials credentials, Flow inputFlow, Flow outputFlow){
+    super(credentials, inputFlow, outputFlow)
+    websocketApi.send(drop(outputFlow.getId()).subscribe({ Drop drop ->
+      if ("waterOn".equals(drop.getFhash()) && drop.getElems().get("command") == true){
+        // The plant has been watered
+        moisture = 50;
+        println "Received water command, resetting moisture"
+      }
+    }));
+  }
+
+  @Override
+  public void run() {
+    try {
+      // Decrease moisture
+      moisture = Math.max(0, moisture - 5);
+
+      // Send
+      println "Sending Moisture: ${moisture}"
+      websocketApi.send(drop(inputFlow.getId()).create(moistureData())).get()
+    } catch (Exception e) {
+      e.printStackTrace()
+    }
+  }
+
+  def moistureData() {
+    return new Drop.Builder().setFhash("soilMoisture").addElem("reading",moisture).get()
+  }
 }
 
 /**
  * Task to send simulated Temperature Sensor readings
  */
 class TemperatureSimulator extends Simulator {
-	
-	def temperature = 60;
-	def random = new Random()
-	
-	def TemperatureSimulator(Credentials credentials, Flow inputFlow, Flow outputFlow){
-		super(credentials, inputFlow, outputFlow)
-	}
-	
-	@Override
-	public void run() {
-		try {
-			// Vary temperature
-			temperature = temperature + random.nextInt(10) - 5 
-			temperature = Math.max(30, Math.min(temperature, 80));
-			
-			// Send
-			println "Sending Temperature: ${temperature}"
-			websocketApi.send(drop(inputFlow.getId()).create(temperatureData())).get()
-		} catch (Exception e) {
-			e.printStackTrace()
-		}
-	}
-	
-	def temperatureData() {
-		return new Drop.Builder().setFhash("temperature").addElem("reading",temperature).get()
-	}
+
+  def temperature = 60;
+  def random = new Random()
+
+  def TemperatureSimulator(Credentials credentials, Flow inputFlow, Flow outputFlow){
+    super(credentials, inputFlow, outputFlow)
+  }
+
+  @Override
+  public void run() {
+    try {
+      // Vary temperature
+      temperature = temperature + random.nextInt(11) - 5
+      temperature = Math.max(30, Math.min(temperature, 80));
+
+      // Send
+      println "Sending Temperature: ${temperature}"
+      websocketApi.send(drop(inputFlow.getId()).create(temperatureData())).get()
+    } catch (Exception e) {
+      e.printStackTrace()
+    }
+  }
+
+  def temperatureData() {
+    return new Drop.Builder().setFhash("temperature").addElem("reading",temperature).get()
+  }
 }
 
 /**
  * Task to send simulated Rain Forecast readings
  */
 class RainForecastSimulator extends Simulator {
-	
-	def rainForecasted = false;
-	def random = new Random()
-	
-	def RainForecastSimulator (Credentials credentials, Flow inputFlow, Flow outputFlow){
-		super(credentials, inputFlow, outputFlow)
-	}
-	
-	@Override
-	public void run() {
-		try {
-			// Flip rain forecast (20% chance of doing so)?
-			def shouldFlip = random.nextInt(100) > 80;
-			rainForecasted = rainForecasted ^ shouldFlip
-			
-			// Send
-			println "Sending Rain Forecasted: ${rainForecasted}"
-			websocketApi.send(drop(inputFlow.getId()).create(forecastData())).get()
-		} catch (Exception e){
-			e.printStackTrace();
-		}
-	}
-	
-	def forecastData() {
-		return new Drop.Builder().setFhash("rainForecasted").addElem("reading",rainForecasted).get()
-	}
+
+  def rainForecasted = false;
+  def random = new Random()
+
+  def RainForecastSimulator (Credentials credentials, Flow inputFlow, Flow outputFlow){
+    super(credentials, inputFlow, outputFlow)
+  }
+
+  @Override
+  public void run() {
+    try {
+      // Flip rain forecast (20% chance of doing so)?
+      def shouldFlip = random.nextInt(100) > 80;
+      rainForecasted = rainForecasted ^ shouldFlip
+
+      // Send
+      println "Sending Rain Forecasted: ${rainForecasted}"
+      websocketApi.send(drop(inputFlow.getId()).create(forecastData())).get()
+    } catch (Exception e){
+      e.printStackTrace();
+    }
+  }
+
+  def forecastData() {
+    return new Drop.Builder().setFhash("rainForecasted").addElem("reading",rainForecasted).get()
+  }
 }
 
 abstract class Simulator implements Runnable {
-	
-	def websocketApi;
-	def inputFlow;
-	def outputFlow;
-	
-	def Simulator(Credentials credentials, Flow inputFlow, Flow outputFlow){
-		this.websocketApi = new WebsocketApi(credentials);
-		this.inputFlow = inputFlow;
-		this.outputFlow = outputFlow;
-	}
+
+  def websocketApi;
+  def inputFlow;
+  def outputFlow;
+
+  def Simulator(Credentials credentials, Flow inputFlow, Flow outputFlow){
+    this.websocketApi = new WebsocketApi(credentials);
+    this.inputFlow = inputFlow;
+    this.outputFlow = outputFlow;
+  }
 }
--- grails-app/views/agriculture/index.gsp
+++ grails-app/views/agriculture/index.gsp
@@ -1,82 +1,138 @@
 <!DOCTYPE html>
 <html>
-	<head>
-		<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-		<title>flowthings.io agriculture app</title>
-	</head>
-	<body>
-		<div><span>Rain Forecasted? </span><span id="rainForecasted"></span></div>
-		<div><span>Temperature: </span><span id="temperature"></span></div>
-		<div><span>Soil Moisture: </span><span id="soilMoisture"></span></div>
-		<div><span>Water On? </span><span id="waterOn"></span></div>
-	</body>
-	<script language="JavaScript">
-		var connection;
+  <head>
+    <link rel='stylesheet' href='css/bootstrap.css'/>
+    <link rel='stylesheet' href='http://fonts.googleapis.com/css?family=Source+Sans+Pro' type='text/css'/>
+    <link rel='shortcut icon' href='favicon.ico'/>
+    <link rel='stylesheet' href='css/style.css'/>
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src='js/justgage.1.0.1.min.js'></script>
+    <script src='js/raphael.2.1.0.min.js'></script>
+    <title>flowthings.io agriculture app</title>
+  </head>
+  <body>
+    <div>
+      <div class="text-center">
+        <a style="display:inline-block;" target="_blank" href="http://flowthings.io" >
+        <object type="image/svg+xml" data="images/flowthings.svg">     
+          <img src="images/flowthings.png"/>     
+        </object>
+        </a> 
+      </div>
+      <h2 class="text-center" style="margin-top: 5px">Grails Smart-Agriculture demo</h2>
+    </div>
+  
+    <div style="width:300px; margin-left:auto; margin-right:auto">
+      <div id="tempGage" style="width:300px;height:200px"></div>
+      <div id="moistureGage" style="width:300px;height:200px"></div>
+      <div class="text-center">
+        <span style="font-weight:bold;font-size:20px">Rain Forecasted? </span>
+        <div id="rainForecasted" class="text-result">No</div></div>
+      <div class="text-center">
+        <span style="font-weight:bold;font-size:20px">Water On? </span>
+        <div id="waterOn" class="text-result">No</div></div>
+    </div>
+  </body>
+  <script>
+    var connection;
 
-		/**
-		 * Send a periodic heartbeat to the WS connection, to
-		 * keep it alive
-		 */
-		function heartbeatWS() {
-		  connection.send(JSON.stringify({
-		    "type": "heartbeat"
-		  }));
-		}
-	
-		function subscribe(path){
-			return JSON.stringify({
-                "msgId": "subscribe-request",
-                "object": "drop",
-                "type": "subscribe",
-                "path": path
-            });
-		}
+    var tempGage;
+    var moistureGage;
 
-		/**
-		 * Set up the WebSocket connection
-		 */
-		$.ajax({
-			url: "https://ws.flowthings.io/session",
-	        beforeSend: function(req) {
-	        	req.setRequestHeader("X-Auth-Account", "${account}");
-	        	req.setRequestHeader("X-Auth-Token", "${token}");
-	        	req.withCredentials = true
-	        },
-	        type: "post",
-	        dataType: 'json',
-	        success: function(data) {
-	
-	          var sessionId = data["body"]["id"]
-	          var url = "wss://ws.flowthings.io/session/" + sessionId + "/ws";
-	
-	          connection = new WebSocket(url);
-	
-	          connection.onopen = function() {
+    var gageFontColor = "#ffffff";
 
-	        	/**
-	      		 * Subscribe to input and output events, so we can
-	      		 * render them in the GUI
-	      		 */
-	            connection.send(subscribe("${inputPath}"));
-	            connection.send(subscribe("${outputPath}"));
-	            var counter = setInterval(heartbeatWS, 10000);
-	          };
-	          connection.onerror = function(error) {
-	            console.log('WebSocket Error ' + error);
-	          };
-	          connection.onmessage = function(e) {
-	            var message = JSON.parse(e.data);
-	            if (message.value){
+    tempGage = new JustGage({
+      id: 'tempGage',
+      titleFontColor: gageFontColor,
+      valueFontColor: gageFontColor,
+      value: 0,
+      min: 30,
+      max: 80,
+      title: 'Temperature'
+    });
+    moistureGage = new JustGage({
+      id: 'moistureGage',
+      titleFontColor: gageFontColor,
+      valueFontColor: gageFontColor,
+      value: 0,
+      min: 0,
+      max: 50,
+      title: 'Soil Moisture'
+    });
+   
 
-	            	/**
-	        		 * When an event is received, render it on screen
-	        		 */
-					var category = message.value.fhash;
-					var value = category === "waterOn" ? message.value.elems.command.value : message.value.elems.reading.value;
-					$("#" + category).text(value)	
-	            }            
-	          };
-	        }
-	      });
-	</script>
+    
+    /**
+     * Send a periodic heartbeat to the WS connection, to
+     * keep it alive
+     */
+    function heartbeatWS() {
+      connection.send(JSON.stringify({
+        "type": "heartbeat"
+      }));
+    }
+  
+    function subscribe(path){
+      return JSON.stringify({
+    	  "msgId": "subscribe-request",
+        "object": "drop",
+        "type": "subscribe",
+        "path": path
+      });
+    }
+
+    /**
+     * Set up the WebSocket connection
+     */
+    $.ajax({
+      url: "https://ws.flowthings.io/session",
+          beforeSend: function(req) {
+            req.setRequestHeader("X-Auth-Account", "${account}");
+            req.setRequestHeader("X-Auth-Token", "${token}");
+            req.withCredentials = true
+          },
+          type: "post",
+          dataType: 'json',
+          success: function(data) {
+  
+            var sessionId = data["body"]["id"]
+            var url = "wss://ws.flowthings.io/session/" + sessionId + "/ws";
+  
+            connection = new WebSocket(url);
+  
+            connection.onopen = function() {
+
+            /**
+              * Subscribe to input and output events, so we can
+              * render them in the GUI
+              */
+            connection.send(subscribe("${inputPath}"));
+            connection.send(subscribe("${outputPath}"));
+            var counter = setInterval(heartbeatWS, 10000);
+          };
+          connection.onerror = function(error) {
+            console.log('WebSocket Error ' + error);
+          };
+          connection.onmessage = function(e) {
+          var message = JSON.parse(e.data);
+          if (message.value){
+
+            /**
+              * When an event is received, render it on screen
+              */
+            var category = message.value.fhash;
+            var value = category === "waterOn" ? message.value.elems.command.value : message.value.elems.reading.value;
+
+            if (category === "temperature"){
+              tempGage.refresh(value);
+            } else if (category === "soilMoisture"){
+              moistureGage.refresh(value);  
+            } else {
+              $("#" + category).text(value ? "Yes" : "No")
+            }  
+          }            
+        };
+      }
+    });
+  </script>
 </html>
